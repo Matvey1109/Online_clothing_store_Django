5bbe7ffa751a5c82fabcfd4c83b946984f054a56
Matvey Kosyakov <matveykos04@gmail.com>
2023-03-20 20:39:12 +0300

    added app, created templates, basic views and urls

---
+++ store/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+	path('categories/', views.categories, name="categories"),
+	path('liked/', views.liked, name="liked"),
+	path('profile/', views.profile, name="profile"),
+]
---
+++ store/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+def store(request):
+    context = {}
+    return render(request, 'store/store.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context)
+
+def categories(request):
+    context = {}
+    return render(request, 'store/categories.html', context)
+
+def liked(request):
+    context = {}
+    return render(request, 'store/liked.html', context)
+
+def profile(request):
+    context = {}
+    return render(request, 'store/profile.html', context)
