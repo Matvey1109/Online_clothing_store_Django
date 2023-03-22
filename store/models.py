4f1b1c67f7c61bc3d96cf5d3b39510b50b12a3e3
Dmitry <dimonchechulov@gmail.com>
2023-03-22 22:15:06 +0300

    Added Migrations

--- store/models.py
+++ store/models.py
@@ -1,4 +1,30 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    fio = models.CharField(max_length=255)
+    adress = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=13)
+
+
+class Product(models.Model):
+
+    AVAILABILITY =(
+        ('Y', 'В наличии'),
+        ('N', 'Нет в наличии'),
+    )
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    photo = models.ImageField(upload_to="photos/")
+    size = models.CharField(max_length=255)
+    availability = models.CharField(max_length=1,choices = AVAILABILITY,default = '')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
+    gender = models.ForeignKey('Gender', on_delete=models.PROTECT)
+    def __str__(self):
+        return self.name
 
     def get_absolute_url(self):
         return reverse('', kwargs={'product_slug': self.slug})#добавить путь /gender/category
