18c5bd51966f5c43493b564631d7880427e05139
Dmitry <dimonchechulov@gmail.com>
2023-05-19 21:32:37 +0300

    Test Deploy

--- internet_shop/settings.py
+++ internet_shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ovs)=+-*eaphebg-10dp$pt9avhf)4$*os+iaj(1ua9z(d7!$b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,7 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
